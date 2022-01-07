6d8b759948ed5a160d4fdb4c8ae2a0bf98e3cd87
Richard Panman <rpanman@sonatype.com>
2022-01-07 10:54:45 +0000

    Removed unused imports

--- src/main/java/org/sonatype/cs/metrics/controller/ApplicationEvaluationsController.java
+++ src/main/java/org/sonatype/cs/metrics/controller/ApplicationEvaluationsController.java
@@ -1,7 +1,5 @@
 package org.sonatype.cs.metrics.controller;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ApplicationEvaluationsController {
